842abd143a777ec6fd5108709b4cc4f1f0250741
nereazj544 <nzjvirtual@gmail.com>
2025-06-18 12:13:16 +0200

    Agregar registros de conexión y manejo de errores en la interfaz de usuario del cliente

--- client_interfaz.py
+++ client_interfaz.py
@@ -1,11 +1,16 @@
 import tkinter
-import customtkinter
+import customtkinter 
 import socket
 import threading
 from Logs_Interfaz import log_critical, log_error, log_info, log_warning, log_debug
 
 HOST = "localhost"  # Dirección del servidor
 PORT = 8083  # Puerto del servidor
+
+# TODO: Configuración GLOBAL socket del cliente
+client_socket = None  # Variable para el socket del cliente, se inicializa como None
+ACTIVO = False  # Variable para indicar si el cliente está activo, se inicializa como False
+
 
 
 
@@ -29,10 +34,20 @@ def button_send():
     customtkinter.CTkMessageBox.show_info("Información", "Mensaje enviado al servidor")  # Mostrar un mensaje de información
 
 
+def connect():
+    global client_socket, ACTIVO  # Acceder a las variables globales
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Crear un socket
+        client_socket.connect((HOST, PORT))  # Conectar al servidor
+        ACTIVO = True  # Cambiar el estado a activo
+        log_info("Conexión establecida con el servidor")  # Registrar la conexión exitosa
+        text.insert(tkinter.END, "Conectado al servidor\n")  # Insertar texto en el cuadro de texto
+        log_info("Conectado al servidor")  # Registrar la conexión exitosa
+    except Exception as e:
+        text.insert(tkinter.END, f"Error al conectar: {e}\n")
+        log_error(f"Error al conectar: {e}")  # Registrar el error
 
-
-
-
+# TODO: Area de mensaje
 text = customtkinter.CTkTextbox(master=app, width=660, height=353)  # Crear un cuadro de texto
 text.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)  # Colocar el cuadro de texto en el centro de la ventana
 
